3ccc5fdbf880ad5e62471a29afd4023aae4510b6
Sindy <sindyxin1@gmail.com>
2018-05-01 01:23:45 -0700

    project construction

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
+// const passportConfig = require("./services/passport");
+// const authRoutes = require("./routes/authRoutes");
+// authRoutes(app);
+require("./services/passport");
+require("./routes/authRoutes")(app);
+
+
+
+
+
+
 const PORT = process.env.PORT || 5000;
-
-app.get('/', (req, res)=>{
-    res.send({bye: 'buddy'});
-})
-
 app.listen(PORT);
